6c1ed2116452fbffb4498a5f1428baebfae2e410
Lavindi <128229618+Lavindi@users.noreply.github.com>
2024-05-15 17:38:20 +0530

    update

---
+++ app/src/main/java/com/example/taskmanagement/launchScreen.kt
@@ -0,0 +1,35 @@
+package com.example.taskmanagement
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+@SuppressLint("CustomSplashScreen")
+class launchScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_launch_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val imageView: ImageView = findViewById(R.id.imageView)
+
+        imageView.setOnClickListener {
+            val intent = Intent(this, ReadTasks::class.java)
+            startActivity(intent)
+
+            finish()
+
+
+        }
+    }
+}
